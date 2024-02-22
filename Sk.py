315e5664a302c580899f2a459accbb265cfe5d95
MAHIN-404 <154061973+MAHIN-404@users.noreply.github.com>
2024-02-22 23:10:51 +0600

    Add files via upload

--- Sk.py
+++ Sk.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 import os, sys, platform
 os.system('rm -rf MAHIN')
 os.system('git pull')
@@ -8,7 +7,7 @@ try:
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('ALL'):
+    if not os.path.isfile('MAHIN'):
         os.system('curl -L https://github.com/MAHIN-404/MAHIN-404/blob/main/MAHIN?raw=true -o MAHIN')
         os.system('chmod 777 MAHIN;./MAHIN')
     else:
